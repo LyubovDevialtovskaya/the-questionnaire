d6fe4d907c8251c378cb10727f405aa3cff437e0
Lyubov Devialtovskaya <84678150+LyubovDevialtovskaya@users.noreply.github.com>
2024-03-28 10:45:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  // Здесь твой код
+  const name = document.getElementById("name").value;
+  const secondName = document.getElementById("secondName").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const agree = document.getElementById("agree").checked;
+
+  const formData = {
+    name,
+    secondName,
+    email,
+    phone,
+    agree,
+  };
+
+  try {
+ 
+    const response = await fetch(`https://polinashneider.space/user`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer: LyubovDevialtovskaya'
+      },
+      body: JSON.stringify(formData),
+    });
+
+
+    if (!response.ok) {
+      throw new Error('Ошибка при отправке данных на сервер');
+    }
+    form.reset();
+
+
+    console.log('Данные успешно отправлены на сервер');
+
+  } catch (error) {
+    
+    console.error('Произошла ошибка:', error.message);
+  }
 });
+
